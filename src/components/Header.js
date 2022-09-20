02e010d0afaa5e20ffaa7aca3decec8ec7874e61
0pilatos0 <Paulvanderlei@hotmail.nl>
2022-09-20 23:46:21 +0200

    WIP homepage

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Paul van der Lei</h1>
+      <h2>cool text or whatever</h2>
+    </header>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="/" className="site-title">
+        Paul van der Lei
+      </a>
+      <ul>
+        <li>
+          <a href="mailto:paulvanderlei@hotmail.nl">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
